f2898a174136c071fb9311975ac560e5a34bc468
xp <xp@fandora.co>
2017-05-25 10:50:26 +0800

    introduce webpack

--- api/index.js
+++ api/index.js
@@ -8,6 +8,19 @@ const cookieSession = require('cookie-session');
 const { GraphQLEnumType, GraphQLID } = require('graphql');
 const { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLList } = require('graphql');
 
+/////////////////////////////////////////////////////////
+
+const app = express();
+app.set('trust proxy', true);
+app.use(morgan('combined'));
+app.use(cookieSession({
+	name : 'session',
+	keys : ['TEST0123456789'],
+	maxAge : 24 * 60 * 60 * 1000 * 365,
+}));
+
+/////////////////////////////////////////////////////////
+
 const EnumTODOStatus = new GraphQLEnumType({
 	name : 'TODOStatus',
 	values : {
@@ -102,14 +115,34 @@ const schema = new GraphQLSchema({
 	}),
 });
 
-const app = express();
-app.set('trust proxy', true);
-app.use(morgan('combined'));
-app.use(cookieSession({
-	name : 'session',
-	keys : ['TEST0123456789'],
-	maxAge : 24 * 60 * 60 * 1000 * 365,
-}));
+//////////////////////////////////////////////////////////
+
+let JS_VERSION = moment().format('YYYYMMDDHH');
+require('fs').readFile(__dirname + '/../web/dist/app.js', 'utf8', (err, data) => {
+	if(data) JS_VERSION = md5(data);
+});
+
+function md5 (str) {
+	return require('crypto').createHash('md5').update(str, 'utf8').digest('hex');
+}
+
+app.get('*', (req, res, next) => {
+	require('fs').readFile(__dirname + '/../web/dist/app.ejs', (err, result) => {
+		if(err) return next(err);
+		result = result.toString();
+		result = require('ejs').render(result, {
+			jsver : JS_VERSION,
+		});
+		res.status(200);
+		res.set({
+			'Content-Type' : 'text/html; charset=UTF-8',
+		});
+		return res.send(result);
+	});
+});
+
+//////////////////////////////////////////////////////////
+
 app.use('/graphql', graphqlHTTP({
 	schema : schema,
 	graphiql : true,
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+const uglify_plugin = new webpack.optimize.UglifyJsPlugin({
+	compress : {
+		warnings : false,
+		unsafe : false,
+	},
+	mangle : true,
+	beautify : false,
+	sourceMap: false,
+});
+
+const plugins = [
+	new LiveReloadPlugin(),
+	// uglify_plugin,
+];
+
+if(process.env.NODE_ENV == 'production') {
+	plugins.push(new webpack.DefinePlugin({
+		'process.env' : {
+			'NODE_ENV' : JSON.stringify('production'),
+		},
+	}));
+	plugins.push(uglify_plugin);
+}
+
+module.exports = {
+	entry : {
+		'app' : [
+			'babel-polyfill',
+			'./web/index.jsx',
+		],
+	},
+	output : {
+		path : path.resolve(__dirname, 'web/dist'),
+		filename : "[name].js",
+	},
+	resolve : {
+		extensions : ['', '.js', '.jsx'],
+		// alias: { },
+	},
+	module : {
+		loaders : [
+			{
+				test : /\.js|jsx$/,
+				loaders : ['babel?cacheDirectory'],
+				exclude : [/node_modules/, /^backend/, /^tests/],
+			},
+			{
+				test : /\.less$/,
+				loader : 'style-loader!css-loader!postcss-loader!less-loader',
+			},
+			{
+				test : /\.css$/,
+				loader : 'style-loader!css-loader!postcss-loader',
+				// loaders : ["style-loader", "css-loader", "postcss-loader"],
+			},
+		],
+		noParse : /node_modules\/quill\/dist/
+	},
+	plugins : plugins,
+};
